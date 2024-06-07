e4d0263bf676d15bd9e1b952e3a49c3fe41213e2
Renan Pinho <pinhorenan@icloud.com>
2024-06-07 05:04:37 -0300

    Added header behavior script and refactored scrolling scripts

--- scripts/hide-see-more.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    const seeMoreContainer = document.getElementById('see-more-container');
-    const sectionGreetings = document.getElementById('section-greetings');
-    const sectionGreetingsBottom = sectionGreetings.getBoundingClientRect().bottom;
-
-    if (sectionGreetingsBottom < 0) {
-        seeMoreContainer.classList.add('hidden');
-    }
-});
